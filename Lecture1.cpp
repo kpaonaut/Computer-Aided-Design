9619d1224c4bbb7a518e09ad3f648ae42b7b12c8
ArayCHN <1172094131@qq.com>
2018-10-07 08:51:38 +0800

    initial commit

---
+++ Lecture1.cpp
@@ -0,0 +1,62 @@
+//#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <stdio.h>
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+
+int main()
+{
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // This sentence is added by Rui
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetKeyCallback(window, key_callback);
+
+	// if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	// {
+	// 	return -1;
+	// }
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
---
+++ glfw-3.2.1/src/CMakeFiles/glfw.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/WangRui/Programming/CAD/glfw-3.2.1/src/cocoa_init.m" "/Users/WangRui/Programming/CAD/glfw-3.2.1/src/CMakeFiles/glfw.dir/cocoa_init.m.o"
+  "/Users/WangRui/Programming/CAD/glfw-3.2.1/src/cocoa_joystick.m" "/Users/WangRui/Programming/CAD/glfw-3.2.1/src/CMakeFiles/glfw.dir/cocoa_joystick.m.o"
+  "/Users/WangRui/Programming/CAD/glfw-3.2.1/src/cocoa_monitor.m" "/Users/WangRui/Programming/CAD/glfw-3.2.1/src/CMakeFiles/glfw.dir/cocoa_monitor.m.o"
+  "/Users/WangRui/Programming/CAD/glfw-3.2.1/src/cocoa_time.c" "/Users/WangRui/Programming/CAD/glfw-3.2.1/src/CMakeFiles/glfw.dir/cocoa_time.c.o"
+  "/Users/WangRui/Programming/CAD/glfw-3.2.1/src/cocoa_window.m" "/Users/WangRui/Programming/CAD/glfw-3.2.1/src/CMakeFiles/glfw.dir/cocoa_window.m.o"
+  "/Users/WangRui/Programming/CAD/glfw-3.2.1/src/context.c" "/Users/WangRui/Programming/CAD/glfw-3.2.1/src/CMakeFiles/glfw.dir/context.c.o"
+  "/Users/WangRui/Programming/CAD/glfw-3.2.1/src/init.c" "/Users/WangRui/Programming/CAD/glfw-3.2.1/src/CMakeFiles/glfw.dir/init.c.o"
+  "/Users/WangRui/Programming/CAD/glfw-3.2.1/src/input.c" "/Users/WangRui/Programming/CAD/glfw-3.2.1/src/CMakeFiles/glfw.dir/input.c.o"
+  "/Users/WangRui/Programming/CAD/glfw-3.2.1/src/monitor.c" "/Users/WangRui/Programming/CAD/glfw-3.2.1/src/CMakeFiles/glfw.dir/monitor.c.o"
+  "/Users/WangRui/Programming/CAD/glfw-3.2.1/src/nsgl_context.m" "/Users/WangRui/Programming/CAD/glfw-3.2.1/src/CMakeFiles/glfw.dir/nsgl_context.m.o"
+  "/Users/WangRui/Programming/CAD/glfw-3.2.1/src/posix_tls.c" "/Users/WangRui/Programming/CAD/glfw-3.2.1/src/CMakeFiles/glfw.dir/posix_tls.c.o"
+  "/Users/WangRui/Programming/CAD/glfw-3.2.1/src/vulkan.c" "/Users/WangRui/Programming/CAD/glfw-3.2.1/src/CMakeFiles/glfw.dir/vulkan.c.o"
+  "/Users/WangRui/Programming/CAD/glfw-3.2.1/src/window.c" "/Users/WangRui/Programming/CAD/glfw-3.2.1/src/CMakeFiles/glfw.dir/window.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "_GLFW_USE_CONFIG_H"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "include"
+  "src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
