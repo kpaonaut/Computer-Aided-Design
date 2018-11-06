325da818c99c56843f138867dadeec62222d97e5
ArayCHN <1172094131@qq.com>
2018-11-06 09:45:57 +0800

    added soil library

---
+++ hw2/main.frag.glsl
@@ -0,0 +1,18 @@
+#version 330 core
+
+in vec2 TexCoords;
+
+out vec4 color;
+
+uniform sampler2D texture1;
+
+void main()
+{
+    //color = texture(texture1, TexCoords);
+    
+    vec4 texColor = texture(texture1, TexCoords);
+    if(texColor.a < 0.1)
+        discard;
+    color = texColor;
+    
+}
